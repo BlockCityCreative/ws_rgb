9303b48ab7856de5d8f219158c768107e5419968
wsor <wsor@gmail.com>
2020-08-30 21:09:28 -0400

    inital commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+minetest.register_node("ws_rgb:solid", {
+    description = "Plastic Box",
+    tiles = {"rgb_solid.png"},
+    is_ground_content = false,
+    groups = {choppy=1, snappy=1, oddly_breakable_by_hand=1, ud_param2_colorable = 1},
+    sounds = default.node_sound_stone_defaults(),
+    paramtype2 = "color",
+    palette = "unifieddyes_palette_extended.png",
+})
